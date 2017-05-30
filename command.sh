83a7343e6dca03efbbbd6be8054e57129306ce86
Chinmayee Shah <chshah@stanford.edu>
2017-05-30 03:33:11 +0000

    sample command

--- command.sh
+++ command.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 --network Squeeze --lr 1e-5 --feature-size 128
+./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 --network Squeeze --lr 1e-5 --feature-size 128 --reg 1e-3
