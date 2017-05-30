4298b734adb0dcdcb19ff08beb7f6ad2f1190c8c
Chinmayee Shah <chshah@stanford.edu>
2017-05-30 04:37:50 +0000

    attempting to run inceptionnet

--- command.sh
+++ command.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 --network Squeeze --lr 1e-5 --feature-size 128 --reg 1e-3
+./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 \
+    --network Inception --lr 5e-4 --feature-size 64 --reg 1e-4 --epochs 12 \
+    --triplet-freq 3 --val-freq 1 --results-freq 1 --test-results-freq 4 \
+    --batch-size 32
+./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 \
+    --network Inception --lr 5e-6 --feature-size 64 --reg 1e-3 --epochs 12 \
+    --triplet-freq 3 --val-freq 1 --results-freq 1 --test-results-freq 4 \
+    --batch-size 32
+./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 \
+    --network Inception --lr 5e-3 --feature-size 64 --reg 1e-4 --epochs 12 \
+    --triplet-freq 3 --val-freq 1 --results-freq 1 --test-results-freq 4 \
+    --batch-size 32
+./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 64 \
+    --network Inception --lr 5e-5 --feature-size 64 --reg 5e-5 --epochs 12 \
+    --triplet-freq 3 --val-freq 1 --results-freq 1 --test-results-freq 4 \
+    --batch-size 32
