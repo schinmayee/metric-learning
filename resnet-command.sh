1b4eac7bc61dc66129dd559817ccdcea1b0b57f0
Chinmayee Shah <chshah@stanford.edu>
2017-06-02 01:06:56 +0000

    more commands

--- resnet-command.sh
+++ resnet-command.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-LOSS=HingeL2
-#LOSS=SquareHingeL2
-#LOSS=Ratio
 ./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 32 \
-    --network ResNet --lr 5e-5 --feature-size 64 --reg 1e-4 --epochs 4 \
+    --network ResNet --lr 1e-6 --feature-size 64 --reg 1e-4 --epochs 4 \
     --triplet-freq 1 --val-freq 1 --results-freq 1 --test-results-freq 1 \
     --batch-size 16  --loss ${LOSS} --mining Hardest \
     --normalize-features
 ./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 32 \
-    --network ResNet --lr 5e-7 --feature-size 64 --reg 1e-3 --epochs 4 \
+    --network ResNet --lr 1e-7 --feature-size 64 --reg 1e-3 --epochs 4 \
     --triplet-freq 1 --val-freq 1 --results-freq 1 --test-results-freq 1 \
     --batch-size 16  --loss ${LOSS} --mining Hardest \
     --normalize-features
@@ -18,12 +15,12 @@ LOSS=HingeL2
     --batch-size 16  --loss ${LOSS}  --mining Hardest \
     --normalize-features
 ./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 32 \
-    --network ResNet --lr 5e-5 --feature-size 64 --reg 1e-4 --epochs 4 \
+    --network ResNet --lr 1e-6 --feature-size 64 --reg 1e-4 --epochs 4 \
     --triplet-freq 1 --val-freq 1 --results-freq 1 --test-results-freq 1 \
     --batch-size 16  --loss ${LOSS} --mining SemiHard \
     --normalize-features
 ./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 32 \
-    --network ResNet --lr 5e-7 --feature-size 64 --reg 1e-3 --epochs 4 \
+    --network ResNet --lr 1e-7 --feature-size 64 --reg 1e-3 --epochs 4 \
     --triplet-freq 1 --val-freq 1 --results-freq 1 --test-results-freq 1 \
     --batch-size 16  --loss ${LOSS} --mining SemiHard \
     --normalize-features
@@ -33,12 +30,12 @@ LOSS=HingeL2
     --batch-size 16  --loss ${LOSS}  --mining SemiHard \
     --normalize-features
 ./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 32 \
-    --network ResNet --lr 5e-5 --feature-size 64 --reg 1e-4 --epochs 4 \
+    --network ResNet --lr 1e-6 --feature-size 64 --reg 1e-4 --epochs 4 \
     --triplet-freq 1 --val-freq 1 --results-freq 1 --test-results-freq 1 \
     --batch-size 16  --loss ${LOSS} --mining KMeans \
     --normalize-features
 ./train.py --num-train 80 --num-val 40 --num-test 80 --triplets-per-class 32 \
-    --network ResNet --lr 5e-7 --feature-size 64 --reg 1e-3 --epochs 4 \
+    --network ResNet --lr 1e-7 --feature-size 64 --reg 1e-3 --epochs 4 \
     --triplet-freq 1 --val-freq 1 --results-freq 1 --test-results-freq 1 \
     --batch-size 16  --loss ${LOSS} --mining KMeans \
     --normalize-features
