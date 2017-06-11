f2e46dfb2f34c2c5f1fcec4ed669e453d44695b7
Chinmayee Shah <chshah@stanford.edu>
2017-06-11 08:01:04 +0000

    tweaks fixes

--- tweaks-command.sh
+++ tweaks-command.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
+#./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
+#    --network Inception --lr 1e-6 --feature-size 64 --reg 1e-3 --epochs 60 \
+#    --results-freq 10 --val-freq 2 --num-batches 10\
+#    --normalize-features  --num-clusters 1 --loss Square
+#./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
+#    --network Inception --lr 1e-6 --feature-size 64 --reg 1e-3 --epochs 60 \
+#    --results-freq 10 --val-freq 2 --num-batches 10\
+#    --normalize-features  --num-clusters 3 --loss Square
+#./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
+#    --network Inception --lr 1e-5 --feature-size 64 --reg 2e-3 --epochs 60 \
+#    --results-freq 10 --val-freq 2 --num-batches 10\
+#    --normalize-features  --num-clusters 1 --loss Soft
+#./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
+#    --network Inception --lr 1e-5 --feature-size 64 --reg 1e-3 --epochs 60 \
+#    --results-freq 10 --val-freq 2 --num-batches 10\
+#    --normalize-features  --num-clusters 3 --loss Soft
+#
+#./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
+#    --network Inception --lr 1e-5 --feature-size 64 --reg 1e-3 --epochs 60 \
+#    --results-freq 10 --val-freq 2 --num-batches 10\
+#    --normalize-features  --num-clusters 3 --local-sampling
 ./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
-    --normalize-features  --num-clusters 1 --loss Square
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
-    --normalize-features  --num-clusters 3 --loss Square
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
-    --normalize-features  --num-clusters 1 --loss Soft
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
-    --normalize-features  --num-clusters 3 --loss Soft
-
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
-    --normalize-features  --num-clusters 3 --local-sampling
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
+    --network Inception --lr 1e-6 --feature-size 64 --reg 1e-3 --epochs 60 \
+    --results-freq 10 --val-freq 2 --num-batches 10\
     --normalize-features  --num-clusters 3 --loss Square  --local-sampling
 ./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64\
-    --network Inception --lr 1e-5 --feature-size 64 --reg 4e-3 --epochs 60 \
-    --results-freq 10 --val-freq 1 --num-batches 10\
+    --network Inception --lr 1e-5 --feature-size 64 --reg 1e-3 --epochs 60 \
+    --results-freq 10 --val-freq 2 --num-batches 10\
     --normalize-features  --num-clusters 3 --loss Soft  --local-sampling
