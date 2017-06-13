645f207649909a88206587ade3aa0b6e33cf4bd1
Chinmayee Shah <chshah@stanford.edu>
2017-06-12 20:04:24 -0700

    renaming

--- report-results/final-hinge-1/command.sh
+++
@@ -1,1 +0,0 @@
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64 --network Inception --lr 4e-5 --feature-size 64 --reg 1e-3 --epochs 100 --results-freq 1 --val-freq 1 --num-batches 10 --normalize-features --num-clusters 1
--- report-results/final-hinge-3-local/command.sh
+++
@@ -1,1 +0,0 @@
-./train_pushed.py --num-train 100 --num-val 40 --num-test 100 --batch-size 64 --network Inception --lr 1e-5 --feature-size 64 --reg 1e-3 --epochs 60 --results-freq 10 --val-freq 2 --num-batches 10 --normalize-features --num-clusters 3 --local-sampling
